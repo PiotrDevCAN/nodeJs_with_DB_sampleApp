5900920e74e5f051fa253497c21ea3adac4f7f8f
Rob Daniel <rob.daniel@uk.ibm.com>
2018-06-21 16:23:44 +0100

    First Commit.

--- app.js
+++ app.js
@@ -26,3 +26,23 @@ app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
   console.log("server starting on " + appEnv.url);
 });
+
+
+
+var schedule = require('node-schedule');
+
+var j = schedule.scheduleJob('00 00 17 * * *', function(){
+  console.log('About to run : vbac-ut.w3ibm.mybluemix.net/batchJobs/revalidate.php');
+		var request = require('request');
+		request('http://vbac-ut.w3ibm.mybluemix.net/batchJobs/revalidate.php', function (error, response, body) {
+		    if (!error && response.statusCode == 200) {
+		    	console.log('Revalidation Successful');
+		        console.log(body) // Print the google web page.
+		     } else {
+		    	 console.log('Revalidation Error');
+		    	 console.log(error);
+		    	 console.log(response);
+		    	 console.log(body);
+		     }
+		}) 
+});
