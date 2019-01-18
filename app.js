f5f88302ae4acd903b83009a02b385bc3b37bee6
R. A. G. Daniel <Rob.Daniel@uk.ibm.com>
2019-01-18 15:04:04 +0000

    15:10

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ var cbnd = schedule.scheduleJob({month: 10, dayOfMonth: 15, hour:03, minute: 00 
 	});
 
 
-var cbnt = schedule.scheduleJob({month: 01, dayOfMonth: 18, hour:14, minute: 50 }, function(){
+var cbnt = schedule.scheduleJob({month: 01, dayOfMonth: 18, hour:15, minute: 10 }, function(){
 	  console.log('About to run : vbac.w3ibm.mybluemix.net/batchJobs/sendCbnEmail.php');
 			var request = require('request');
 			request('http://vbac.w3ibm.mybluemix.net/batchJobs/sendCbnEmail.php', function (error, response, body) {
@@ -193,5 +193,8 @@ var l = schedule.scheduleJob('05 * * * *', function(){
 var m = schedule.scheduleJob('35 * * * *', function(){
 	console.log('Heartbeat at ' + new Date());
 
-});
+})
+
+console.log(m);
+console.log('scheduleJob entries created');
 
