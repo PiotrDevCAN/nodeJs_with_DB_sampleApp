c51def61e55235a9c4d7be3b4f746db896ee666f
Piotr Tajanowicz <piotr.tajanowicz@kyndryl.com>
2023-10-18 14:00:34 +0200

    enable vBAC scheduled jobs

--- app.js
+++ app.js
@@ -49,6 +49,184 @@ rule.minute = 0;
 /*
 * vBAC jobs
 */
+/*
+console.log('About to run vBAC wakeup_slack_message');
+var request = require('request');
+request(vbac_url + 'batchJobs/wakeup_slack_message.php', function (error, response, body) {
+	if (!error && response.statusCode == 200) {
+		console.log('wakeup message successful');
+		console.log(body); // Print the google web page.
+	} else {
+		console.log('wakup message error');
+		console.log(error);
+		console.log(response);
+		console.log(body);
+	}
+});
+*/
+
+var l = schedule.scheduleJob({ hour: 2, minute: 55 }, function () {
+	console.log('About to run vBAC recheckPotentialLeavers');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/recheckPotentialLeavers.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('Recheck Successful');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('Recheck Error');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var pesB = schedule.scheduleJob({ hour: 3, minute: 13, dayOfWeek: 0 }, function () {
+	console.log('About to run vBAC pesRecheckNotification');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/pesRecheckNotification.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('PES Recheck Successful');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('PES Recheck Error');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var r = schedule.scheduleJob({ hour: 3, minute: 29 }, function () {
+	console.log('About to run vBAC revalidate');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/revalidate.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('PES Cleared Revalidation Successful');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('PES Cleared Revalidation Error');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var updUPES = schedule.scheduleJob({ hour: 4, minute: 30 }, function () {
+	console.log('About to run vBAC updatePesFieldsFromUpes');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/updatePesFieldsFromUpes.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('PES Update from UPES Successful');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('PES Update from UPES Error');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var updKPES = schedule.scheduleJob({ hour: 5, minute: 01 }, function () {
+	console.log('About to run vBAC updatePesFieldsFromUpesKyndryl');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/updatePesFieldsFromUpesKyndryl.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('PES Update from KPES Successful');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('PES Update from KPES Error');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+/*
+var ilc = schedule.scheduleJob({hour: 10, minute: 00, dayOfWeek: 5}, function(){
+	console.log('About to run vBAC ilcReminder');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/ilcReminder.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('ilc Reminder Sent');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('Error sending ilc Reminder');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+*/
+
+var cbna = schedule.scheduleJob({ month: 0, date: 15, hour:03, minute: 01 }, function () {
+	console.log('About to run vBAC sendCbnEmail');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/sendCbnEmail.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('CBN Sent');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('Error sending CBN');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var cbnb = schedule.scheduleJob({ month: 3, date: 15, hour:03, minute: 01 }, function () {
+	console.log('About to run vBAC sendCbnEmail');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/sendCbnEmail.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('CBN Sent');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('Error sending CBN');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var cbnc = schedule.scheduleJob({ month: 6, date: 15, hour:03, minute: 01 }, function () {
+	console.log('About to run vBAC sendCbnEmail');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/sendCbnEmail.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('CBN Sent');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('Error sending CBN');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
+var cbnd = schedule.scheduleJob({ month: 9, date: 15, hour:03, minute: 01 }, function () {
+	console.log('About to run vBAC sendCbnEmail');
+	var request = require('request');
+	request(vbac_url + 'batchJobs/sendCbnEmail.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('CBN Sent');
+			console.log(body); // Print the google web page.
+		} else {
+			console.log('Error sending CBN');
+			console.log(error);
+			console.log(response);
+			console.log(body);
+		}
+	});
+});
+
 var ed = schedule.scheduleJob(rule, function(){
 	console.log('About to run vBAC employeeData');
 	var today = new Date();
@@ -89,6 +267,24 @@ var restVBACActivePersons = schedule.scheduleJob({ hour: 1, minute: 45 }, functi
 		}
 	});
 });
+
+/*
+var restAutoClose = schedule.scheduleJob({ hour: 1, minute: 15 }, function () {
+	console.log('About to run REST Autoclose');
+	var request = require('request');
+	request(rest_url + 'batchJobs/autoClose.php', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			console.log('Autoclose Run');
+			console.log(body); // Print whatever came back.
+		} else {
+			console.log('Error running autoclose');
+			// console.log(error);
+			// console.log(response);
+			console.log(body);
+		}
+	});
+});
+*/
 
 /*
 var restSendRFSData = schedule.scheduleJob(rule, function(){
